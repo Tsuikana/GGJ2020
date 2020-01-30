d829b5b9ab66052c5bec59e9c89ec599abf96c0b
Kevin Chung <Tsuikana@gmail.com>
2020-01-29 23:07:46 -0800

    Adding base girl stats thingy
    DESCRIPTION

---
+++ Assets/Scripts/GirlStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlStats : MonoBehaviour
+{
+    public float happiness;
+    public float hunting;
+    public float gathering;
+    public float scouting;
+    public float mobility;
+
+    public string element;
+    public string type;
+}
