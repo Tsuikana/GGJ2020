d913b68830474bccc4125c98bc615ed5725dc48d
Alvin <alvin216@gmail.com>
2020-02-02 14:42:22 -0800

    Added Win/Lose states

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,10 @@ public class GameManager : MonoBehaviour
     public UIManager uiMan;
     public Interaction interaction;
     public GirlsSpawner gSpawn;
+    public GameObject prefabWinTrigger;
+    public GameObject winImage;
+    public GameObject loseImage;
+    private GameObject sceneWinTrigger;
     private Vector3 startPoint;
     private Vector3 endPoint;
 
@@ -30,16 +34,29 @@ public class GameManager : MonoBehaviour
         FindObjectOfType<MovementControllerMc>().gameObject.transform.position = new Vector3(startPoint.x + 0.05f, startPoint.y + 0.35f, 0f);
         gSpawn = GetComponent<GirlsSpawner>();
         gSpawn.SpawnGirls();
+
+        sceneWinTrigger = Instantiate(prefabWinTrigger);
+        sceneWinTrigger.transform.position = endPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    } 
+    }
 
-    void EndGame(string deathReason)
+    public void WinGame()
     {
-        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200, 50), deathReason);
+        Debug.Log("YOU WIN!!!!!!");
+        Time.timeScale = 0;
+        winImage.SetActive(true);
+    }
+
+    public void LoseGame(string deathReason)
+    {
+        Debug.Log("YOU LOSE!!!!!!");
+        Time.timeScale = 0;
+        loseImage.SetActive(true);
+        //GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200, 50), deathReason);
     }
 }
